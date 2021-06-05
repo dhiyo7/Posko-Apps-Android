6629b93cd16783118c32d5a238a3a22e074c376d
dhiyo7 <dhiyoreksa12@gmail.com>
2021-06-06 00:59:49 +0700

    3 - Intent maps app

--- app/src/main/java/com/adit/poskoapp/adapters/PoskoAdapter.kt
+++ app/src/main/java/com/adit/poskoapp/adapters/PoskoAdapter.kt
@@ -1,13 +1,18 @@
 package com.adit.poskoapp.adapters
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.adit.poskoapp.R
 import com.adit.poskoapp.models.Posko
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.list_item_posko.view.*
+
 
 class PoskoAdapter(private var data: List<Posko>, private var context: Context) :
     RecyclerView.Adapter<PoskoAdapter.MyHolder>() {
@@ -27,7 +32,17 @@ class PoskoAdapter(private var data: List<Posko>, private var context: Context) 
             itemView.tvNamaPosko.text = posko.nama
             itemView.tvPengungsi.text = posko.jumlah_pengungsi
             itemView.tvLokasi.text = posko.lokasi
-
+            itemView.btnMaps.setOnClickListener {
+                val pcc = LatLng(posko.latitude?.toDouble()!!, posko.longitude?.toDouble()!!)
+                val map = "http://maps.google.com/maps?q=loc:" + posko.latitude
+                    .toString() + "," + posko.longitude.toString() + " (" + posko.nama
+                    .toString() + ")"
+                val gmmIntentUri: Uri = Uri.parse(map) as Uri
+                System.err.println(pcc);
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                context.startActivity(mapIntent)
+            }
         }
 
     }
